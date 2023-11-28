9115684a04b95d7ef6fde544483bb99e8d3c01a5
SergioSusanu <Sergio.susanu.developer@gmail.com>
2023-11-28 15:05:12 +0200

    QuickSort

---
+++ Sorting/QuickSort.js
@@ -0,0 +1,22 @@
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+let array = [12, 11, 15, 10, 9, 1, 2, 3, 13, 14, 4, 5, 6, 7, 8];
+console.log(quickSort(array));
